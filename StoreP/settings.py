7ff437248b7ef8da561dd35f776ba6c210c6835a
Ahmed Mustafa <am1804067@gmail.com>
2023-03-14 22:20:47 +0200

    edit_settings

--- StoreP/settings.py
+++ StoreP/settings.py
@@ -17,24 +17,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_CREDENTIALS = True
-
 CSRF_TRUSTED_ORIGINS = ['https://jewelry-store-production-fca3.up.railway.app/']
-
-CORS_REPLACE_HTTPS_REFERER = True
-
-CSRF_COOKIE_DOMAIN = 'bluemix.net'
-
-CORS_ORIGIN_WHITELIST = (
-    'https://jewelry-store-production-fca3.up.railway.app/',
-    'jewelry-store-production-fca3.up.railway.app',
-    'railway.app',
-)
 
 # Application definition
 
