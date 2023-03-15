2c60a1647d278ec16a96a018c243e6637d929209
Ahmed Mustafa <am1804067@gmail.com>
2023-03-15 10:22:20 +0200

    edit_admin_panel

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from .models import Product, Type, Cart, Buying
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.site_header = "Fefel Jewels"
+
+class InlineProduct(admin.StackedInline):
+    model = Product
+    extra = 1
+
+class TypeAdmin(admin.ModelAdmin):
+    inlines = [InlineProduct]
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('type', 'price', 'left')
+    list_editable = ('name', 'type', 'price')
+    list_filter = ('type', 'on_stock')
+    search_fields = ('name', 'type')
+
+admin.site.register(Product , ProductAdmin)
 admin.site.register(Cart)
 admin.site.register(Type)
 admin.site.register(Buying)
